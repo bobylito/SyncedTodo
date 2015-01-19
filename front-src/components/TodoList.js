83d01b446c16a0cf046f1773c2cf49654850954f
Alexandre <bobylito@gmail.com>
2015-01-19 02:32:37 +0100

    Unique key for todo item

--- front-src/components/TodoList.js
+++ front-src/components/TodoList.js
@@ -4,7 +4,7 @@ var TodoItem = require('./TodoItem');
 var TodoList = React.createClass({
   render : function(){
     var todos = this.props.list.map( function(item){
-      return <TodoItem item={item} />;
+      return <TodoItem item={item} key={item.cid}/>;
     });
     return <ul className="todos">
              {todos}
